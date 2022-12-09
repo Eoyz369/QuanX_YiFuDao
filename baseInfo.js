7ad16a4340144632d7420a0a441813a33284f178
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-09 20:49:58 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -1,8 +1,23 @@
 
 body = JSON.parse($response.body)
 body.data.qrCodeColor ="green"
-body.qrCodeUrl="https://bj.bcebos.com/qr-code/22120919e953ffc480fb.jpg"
+// body.qrCodeUrl="https://bj.bcebos.com/qr-code/22120919e953ffc480fb.jpg"
 
-body.qrCodeOARecord.applyStatus=4
+body.qrCodeOARecord.oaAlias=1
 
 $done({ body: JSON.stringify(body) });
+
+//         <view class="lists lists_bg" style="background-image: url({{detail.qrCodeOARecord.applyStatus}})" wx:if="{{detail.qrCodeOARecord}}">
+//             <view class="list">
+//                 <view class="list_left">类型</view>
+//                 <view class="list_right" style="color:#7ec37b">{{detail.qrCodeOARecord.oaType}}{{detail.qrCodeOARecord.oaAlias?'-'+detail.qrCodeOARecord.oaAlias:''}}</view>
+//             </view>
+//             <view class="list">
+//                 <view class="list_left">开始时间</view>
+//                 <view class="list_right">{{detail.qrCodeOARecord.beginTime}}</view>
+//             </view>
+//             <view class="list">
+//                 <view class="list_left">结束时间</view>
+//                 <view class="list_right">{{detail.qrCodeOARecord.endTime}}</view>
+//             </view>
+//         </view>
