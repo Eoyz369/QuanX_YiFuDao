7f98851236cf3be1bda5d15d9e687d845890c286
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-09 21:02:50 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -1,9 +1,22 @@
 
+
+mt=new Date()
+mt.setMinutes(0)
+bt = mt.getTime()-7200000
+et = mt.getTime()+10800000
+
+
+
+
 body = JSON.parse($response.body)
 body.data.qrCodeColor ="green"
 // body.qrCodeUrl="https://bj.bcebos.com/qr-code/22120919e953ffc480fb.jpg"
 
-body.data.qrCodeOARecord.applyStatus="https://meeting-image-1257318859.cos.ap-beijing.myqcloud.com/htmlImages/yfd/student/postmark/pic-ing.png"
+body.data.qrCodeOARecord.beginTime = bt
+body.data.qrCodeOARecord.endTime = et
+
+
+// body.data.qrCodeOARecord.applyStatus="https://meeting-image-1257318859.cos.ap-beijing.myqcloud.com/htmlImages/yfd/student/postmark/pic-ing.png"
 
 $done({ body: JSON.stringify(body) });
 
