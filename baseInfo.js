cf0ac7b563dff1137fb728d8713ac79682e35683
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-09 19:44:09 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -1,5 +1,8 @@
+
+body = JSON.parse($response.body)
 body.data.qrCodeColor ="green"
 body.qrCodeUrl="https://bj.bcebos.com/qr-code/22120919e953ffc480fb.jpg"
 
 
 
+$done({ body: JSON.stringify(body) });
