b4565f68a8d6da72a75d539d728a36e2cfabe67c
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-09 19:03:58 +0800

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,13 +1,13 @@
 
-mt=new Date()
-mt.setMinutes(0)
-bt=mt.getTime()-7200000
-et=mt.getTime()+10800000
+// mt=new Date()
+// mt.setMinutes(0)
+// bt=mt.getTime()-7200000
+// et=mt.getTime()+10800000
 
 body = JSON.parse($response.body)
 body.data.askForLeaveDetail.applyStatus = 4
-body.data.askForLeaveDetail.beginTime = bt
-body.data.askForLeaveDetail.endTime = et
+// body.data.askForLeaveDetail.beginTime = bt
+// body.data.askForLeaveDetail.endTime = et
 // body.data.askForLeaveDetail.reason = "我想出去吃汉堡王啊！"
 // dt.chains[1].status = 3;
 
@@ -30,7 +30,7 @@ approveDate.setHours(b, Math.floor(Math.random() * 60), 0, 0);
 
 Date.prototype.format=function(fmt){var o={'M+':this.getMonth()+1,'d+':this.getDate(),'h+':this.getHours(),'m+':this.getMinutes(),'s+':this.getSeconds(),'q+':Math.floor((this.getMonth()+3)/3),S:this.getMilliseconds()};if(/(y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(this.getFullYear()+'').substr(4-RegExp.$1.length));}for(var k in o){if(new RegExp('('+k+')').test(fmt)){fmt=fmt.replace(RegExp.$1,RegExp.$1.length==1?o[k]:('00'+o[k]).substr((''+o[k]).length));}}return fmt;};
 
-
+body.data.askForLeaveDetail.chains[0].time = applyDate.format('yyyy-MM-dd hh:mm');
 body.data.askForLeaveDetail.chains[1].time = approveDate.format('yyyy-MM-dd hh:mm');
 
 
