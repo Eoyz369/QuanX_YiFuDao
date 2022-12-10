3288dcf2e4996b2a60b2aa6a5aa49d8b8a46efab
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-10 17:15:44 +0800

    Update status.js

--- status.js
+++ status.js
@@ -1,5 +1,5 @@
 body = JSON.parse($response.body)
-body.data.statusDesc ="出校"
-body.data.oaType ="请假"
+body.data[0].statusDesc ="出校"
+body.data[0].oaType ="请假"
 
 $done({ body: JSON.stringify(body) });
