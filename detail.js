8be667b3fe1434898d3a0c1976742168a1e6c2d2
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-10 17:14:50 +0800

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,15 +1,7 @@
 
-// mt=new Date()
-// mt.setMinutes(0)
-// bt=mt.getTime()-7200000
-// et=mt.getTime()+10800000
 
 body = JSON.parse($response.body)
 body.data.askForLeaveDetail.applyStatus = 4
-// body.data.askForLeaveDetail.beginTime = bt
-// body.data.askForLeaveDetail.endTime = et
-// body.data.askForLeaveDetail.reason = "我想出去吃汉堡王啊！"
-// dt.chains[1].status = 3;
 
 
 body.data.askForLeaveDetail.chains[1].status = 3
