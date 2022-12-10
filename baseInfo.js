cca186c507a396096b47a10524dfb7bc8f1deee0
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-10 17:07:15 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -13,15 +13,13 @@ body.data.qrCodeColor ="green"
 body.data.qrCodeUrl="https://yfd.ly-sky.com/ly-ms/application/open/qrCode/1?t=ms_84&y=1001670642842623004230000000369"
 
 body.data.qrCodeOARecord = {
-"oaType": "离校",
-"oaAlias": "暑假离校",
-"beginTime": 1670738400000,
-"endTime": 1670756400000,
-"applyStatus": 3
+"oaType": "请假",
+"oaAlias": "事假",
+"beginTime": bt,
+"endTime": et,
+"applyStatus": 4
 }
-// body.data.qrCodeOARecord.endTime =et
 
-// body.data.qrCodeOARecord.applyStatus=4
 
 
 
