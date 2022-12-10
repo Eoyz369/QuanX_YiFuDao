5bb91b704e19cfe6d67a7a461153c6ebacb0f851
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-10 17:05:23 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -10,13 +10,19 @@ et = mt.getTime()+10800000
 
 body = JSON.parse($response.body)
 body.data.qrCodeColor ="green"
+body.data.qrCodeUrl="https://yfd.ly-sky.com/ly-ms/application/open/qrCode/1?t=ms_84&y=1001670642842623004230000000369"
 
-
-// body.data.qrCodeOARecord = ""
+body.data.qrCodeOARecord = {
+"oaType": "离校",
+"oaAlias": "暑假离校",
+"beginTime": 1670738400000,
+"endTime": 1670756400000,
+"applyStatus": 3
+}
 // body.data.qrCodeOARecord.endTime =et
 
-body.data.qrCodeOARecord.applyStatus=4
-body.data.qrCodeUrl="https://yfd.ly-sky.com/ly-ms/application/open/qrCode/1?t=ms_84&y=1001670642842623004230000000369"
+// body.data.qrCodeOARecord.applyStatus=4
+
 
 
 $done({ body: JSON.stringify(body) });
