e572f40f0fb10d196b2f9f7955b34191b74851dc
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2022-12-10 16:58:45 +0800

    Update baseInfo.js

--- baseInfo.js
+++ baseInfo.js
@@ -10,28 +10,14 @@ et = mt.getTime()+10800000
 
 body = JSON.parse($response.body)
 body.data.qrCodeColor ="green"
-body.data.qrCodeUrl="https://bj.bcebos.com/qr-code/22120919e953ffc480fb.jpg"
 
-body.data.qrCodeOARecord = ""
+
+// body.data.qrCodeOARecord = ""
 // body.data.qrCodeOARecord.endTime =et
-body.data.qrCodeOARecord.applyStatus="https://meeting-image-1257318859.cos.ap-beijing.myqcloud.com/htmlImages/yfd/student/postmark/pic-ing.png"
 
-
+body.data.qrCodeOARecord.applyStatus=4
+body.data.qrCodeUrl="https://yfd.ly-sky.com/ly-ms/application/open/qrCode/1?t=ms_84&y=1001670642842623004230000000369"
 
 
 $done({ body: JSON.stringify(body) });
 
-//         <view class="lists lists_bg" style="background-image: url({{detail.qrCodeOARecord.applyStatus}})" wx:if="{{detail.qrCodeOARecord}}">
-//             <view class="list">
-//                 <view class="list_left">类型</view>
-//                 <view class="list_right" style="color:#7ec37b">{{detail.qrCodeOARecord.oaType}}{{detail.qrCodeOARecord.oaAlias?'-'+detail.qrCodeOARecord.oaAlias:''}}</view>
-//             </view>
-//             <view class="list">
-//                 <view class="list_left">开始时间</view>
-//                 <view class="list_right">{{detail.qrCodeOARecord.beginTime}}</view>
-//             </view>
-//             <view class="list">
-//                 <view class="list_left">结束时间</view>
-//                 <view class="list_right">{{detail.qrCodeOARecord.endTime}}</view>
-//             </view>
-//         </view>
